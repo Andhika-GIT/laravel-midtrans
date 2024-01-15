df3453c7451771c4858b219d3c8bb0051349248b
Andhika-GIT <andhikapr081@gmail.com>
2024-01-15 14:45:21 +0700

    feat : add temporary total price and status, return response

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -7,5 +7,20 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function checkout(Request $request)
+    {
+        $data = $request->validate([
+            'quantity' => 'required',
+            'name' => 'required',
+            'phone' => 'required',
+            'address' => 'required'
+        ]);
+
+        $data['total_price'] = $request->quantity * 100000;
+        $data['status'] = 'Unpaid';
+
+        return response()->json([
+            'data' => $data
+        ]);
+    }
 }
