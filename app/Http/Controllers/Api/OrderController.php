f07a0413ec9c79b4036e92a0b0e88cbab927c115
Andhika-GIT <andhikapr081@gmail.com>
2024-01-15 22:48:52 +0700

    feat : configure midtrans with client request, and sending midtrans token to client

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -10,17 +11,43 @@ class OrderController extends Controller
     public function checkout(Request $request)
     {
         $data = $request->validate([
-            'quantity' => 'required',
+            'qty' => 'required',
             'name' => 'required',
             'phone' => 'required',
             'address' => 'required'
         ]);
 
-        $data['total_price'] = $request->quantity * 100000;
+        $data['total_price'] = $request->qty * 100000;
         $data['status'] = 'Unpaid';
 
+        $order = Order::create($data);
+
+
+
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $order->id,
+                'gross_amount' => $order->total_price,
+            ),
+            'customer_details' => array(
+                'name' => $request->name,
+                'phone' => $request->phone,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+
         return response()->json([
-            'data' => $data
+            'token' => $snapToken
         ]);
     }
 }
