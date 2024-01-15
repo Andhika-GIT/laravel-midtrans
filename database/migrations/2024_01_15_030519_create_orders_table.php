764759037b117f1fdf0049cc140d70a7201c0539
Andhika-GIT <andhikapr081@gmail.com>
2024-01-15 10:13:18 +0700

    feat : running migration

--- database/migrations/2024_01_15_030519_create_orders_table.php
+++ database/migrations/2024_01_15_030519_create_orders_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('address');
+            $table->string('phone');
+            $table->integer('qty');
+            $table->bigInteger('total_price');
+            $table->enum('status', ['Unpaid', 'Paid']);
             $table->timestamps();
         });
     }
